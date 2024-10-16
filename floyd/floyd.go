1cf8eb12395b7088c212c6cdc3cda306cd2ad6ed
mick4711 <mick4711@gmail.com>
2024-10-17 00:11:32 +0100

    1.23 floyd

--- floyd/floyd.go
+++ floyd/floyd.go
@@ -1,15 +1,16 @@
 package floyd
 
-// Triangle makes a Floyd's triangle matrix with rows count.
-func Triangle(rows int) [][]int {
-	triangle := make([][]int, rows)
-	var i int
-	for r := 0; r < rows; r++ {
-		triangle[r] = make([]int, r+1)
-		for c := 0; c < r+1; c++ {
-			i++
-			triangle[r][c] = i
+import "fmt"
+
+func Triangle(rowCount int) [][]int {
+	for i := range rowCount {
+		outer := [][]int{}
+		inner := []int{}
+		for j := range i {
+			fmt.Println(i, j)
+			inner = []int{1}
 		}
+		append(outer, inner)
 	}
-	return triangle
+	return outer
 }
