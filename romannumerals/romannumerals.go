e844db1594b807165c522a09112f235702c02b6a
mick4711 <mick4711@gmail.com>
2024-10-16 19:26:57 +0100

    start roman numerals

--- romannumerals/romannumerals.go
+++ romannumerals/romannumerals.go
@@ -1,53 +1,81 @@
 package romannumerals
 
-import "strings"
+import (
+	"fmt"
+	"strconv"
+)
 
-type romanArabic struct {
-	roman  string
-	arabic int
+var romanIntValues = map[string]int{
+	"M": 1000,
+	"D": 500,
+	"C": 100,
+	"L": 50,
+	"X": 10,
+	"V": 5,
+	"I": 1,
 }
 
-var numbers = []romanArabic{
-	{"M", 1000},
-	{"CM", 900},
-	{"D", 500},
-	{"CD", 400},
-	{"C", 100},
-	{"XC", 90},
-	{"L", 50},
-	{"XL", 40},
-	{"X", 10},
-	{"IX", 9},
-	{"V", 5},
-	{"IV", 4},
-	{"I", 1},
+var integerRomValues = map[string]string{
+	"1": "I",
+	"2": "500",
 }
 
-func Encode(n int) (string, bool) {
-	var sb strings.Builder
-	for _, ra := range numbers {
-		for n >= ra.arabic {
-			n -= ra.arabic
-			sb.WriteString(ra.roman)
+func Encode(num int) (rom string, ok bool) {
+	if num < 1 {
+		return "", false
+	}
+	numString := strconv.Itoa(num)
+
+	// romString := ""
+	switch numLen := len(numString); {
+	case numLen > 3: // thousands
+		fmt.Println(num, "thousands")
+	case numLen > 2: // hundreds
+		fmt.Println(num, "hundreds")
+	case numLen > 1: // tens
+		fmt.Println(num, "tens")
+	default: // units
+		fmt.Println(num, "units")
+
+	}
+	return "", false
+}
+
+func Decode(rom string) (int, bool) {
+	num := 0
+	ok := false
+
+	for i, s := range rom {
+		var intValue int
+		romNumeral := string(s)
+
+		// read integer value of roman letter from map
+		intValue, ok = romanIntValues[romNumeral]
+		if !ok {
+			return 0, ok
 		}
-	}
 
-	return sb.String(), sb.Len() > 0
-}
-
-func Decode(s string) (int, bool) {
-	result := 0
-
-	for _, ra := range numbers {
-		for strings.HasPrefix(s, ra.roman) {
-			s = s[len(ra.roman):]
-			result += ra.arabic
+		// check for subtractive preceding letter
+		if i > 0 {
+			switch romNumeral {
+			case "V", "X":
+				if string(rom[i-1]) == "I" {
+					intValue -= 2
+				}
+			case "L", "C":
+				if string(rom[i-1]) == "X" {
+					intValue -= 20
+				}
+			case "D", "M":
+				if string(rom[i-1]) == "C" {
+					intValue -= 200
+				}
+			}
 		}
+
+		// accumulate value
+		num += intValue
 	}
 
-	if len(s) != 0 {
-		return 0, false
-	}
-
-	return result, result > 0
+	return num, ok
 }
