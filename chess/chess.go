d9151026debf54f350a8a479f4cb9c1fc11377d9
mick4711 <mick4711@gmail.com>
2024-10-17 23:50:18 +0100

    finish chess

--- chess/chess.go
+++ chess/chess.go
@@ -2,34 +2,51 @@ package chess
 
 import (
 	"errors"
-	"strings"
+	"math"
 )
 
+var invalidSquareErr = errors.New("invalid square")
+
 func CanKnightAttack(white, black string) (bool, error) {
-	// validate entry
 	// square must have 2 characters
 	if len(white) != 2 || len(black) != 2 {
-		return true, errors.New("invalid square")
+		return true, invalidSquareErr
 	}
 
-	// cannot be on the same
+	// cannot be on the same square
 	if white == black {
-		return true, errors.New("invalid square")
+		return false, invalidSquareErr
 	}
-	// parse rank & file
-	files := "abcdefgh"
-	ranks := "12345678"
-	wf := string(white[0])
-	wr := string(white[1])
-	bf := string(black[0])
-	br := string(black[1])
-	if !strings.Contains(files, wf) ||
-		!strings.Contains(files, bf) ||
-		!strings.Contains(ranks, wr) ||
-		!strings.Contains(ranks, br) {
-		return true, errors.New("invalid square")
-	}
-	// check for colour match
 
+	// assign integer values to file letters and rank digits
+	wFile := int(white[0] - 'a' + 1)
+	if wFile < 1 || wFile > 8 {
+		return false, invalidSquareErr
+	}
+
+	bFile := int(black[0] - 'a' + 1)
+	if bFile < 1 || bFile > 8 {
+		return false, invalidSquareErr
+	}
+
+	wRank := int(white[1] - '1' + 1)
+	if wRank < 1 || wRank > 8 {
+		return false, invalidSquareErr
+	}
+
+	bRank := int(black[1] - '1' + 1)
+	if bRank < 1 || bRank > 8 {
+		return false, invalidSquareErr
+	}
+
+	// check file and rank relative positions
+	switch int(math.Abs(float64(wFile - bFile))) {
+	case 1:
+		return int(math.Abs(float64(wRank-bRank))) == 2, nil
+	case 2:
+		return int(math.Abs(float64(wRank-bRank))) == 1, nil
+	}
+
+	// white and black valid but not in knight range of each other
 	return false, nil
 }
--- chess/chess_test.go
+++ chess/chess_test.go
@@ -7,25 +7,25 @@ var tests = []struct {
 	black  string
 	attack bool
 	error  bool
-}{
-	{"not", "valid", true, true},
-	{"", "", false, true},
-	{"a8", "b9", false, true},
-	{"a0", "b1", false, true},
-	{"g3", "i5", false, true},
-	{"b4", "b4", false, true},
-	{"a8", "b6", true, false},
-	{"b7", "d8", true, false},
-	{"c6", "b8", true, false},
-	{"d1", "f2", true, false},
-	{"e3", "g2", true, false},
-	{"f8", "h7", true, false},
-	{"g1", "h3", true, false},
-	{"h4", "g2", true, false},
-	{"f1", "g2", false, false},
-	{"d4", "f4", false, false},
-}
-
+	}{
+		{"f1", "g2", false, false},
+		{"d4", "f4", false, false},
+		{"a8", "b6", true, false},
+		{"b7", "d8", true, false},
+		{"c6", "b8", true, false},
+		{"d1", "f2", true, false},
+		{"e3", "g2", true, false},
+		{"f8", "h7", true, false},
+		{"g1", "h3", true, false},
+		{"h4", "g2", true, false},
+		{"b4", "b4", false, true},
+		{"a8", "b9", false, true},
+		{"a0", "b1", false, true},
+		{"g3", "i5", false, true},
+		{"not", "valid", true, true},
+		{"", "", false, true},
+	}
+	
 func TestCanKnightAttack(t *testing.T) {
 	for _, test := range tests {
 		switch attack, err := CanKnightAttack(test.white, test.black); {
