97b2ae8adbb2fda288a3b93b96d03bd59d68dd79
mick4711 <mick4711@gmail.com>
2024-10-21 00:45:30 +0100

    get it working

--- anagram/anagram.go
+++ anagram/anagram.go
@@ -1,34 +1,64 @@
 package anagram
 
 import (
-	"slices"
+	// "fmt"
+	"sort"
 	"strings"
-	"unicode"
 )
 
-func Normalize(s string) string {
-	var r []rune
-	for _, b := range s {
-		b = unicode.ToLower(b)
-		if b >= 'a' && b <= 'z' {
-			r = append(r, b)
+func FindAnagrams(dictionary []string, word string) []string {
+	// clean word
+	cleanWordInts := cleanInts(word)
+	sort.Ints(cleanWordInts)
+	// fmt.Printf("clean word string %v\n", cleanWordInts)
+
+	res := []string{}
+
+	for index, dicWord := range dictionary {
+		// eliminate if lettercount different
+		cleanDicInts := cleanInts(dicWord)
+		if len(cleanWordInts) != len(cleanDicInts) {
+			continue
+		}
+
+		//TODO cache cleanDicInts keyed by index
+		
+		// eliminate if same word
+		if strings.ToLower(strings.TrimSpace(word)) == strings.ToLower(strings.TrimSpace(dicWord)) {
+			continue
+		}
+
+		sort.Ints(cleanDicInts)
+		// fmt.Printf("possible dictionary word(%v) %v\n", index, cleanDicInts)
+		if areEqual(cleanWordInts, cleanDicInts) {
+			res = append(res, dictionary[index])
 		}
 	}
-	slices.Sort(r)
-	return string(r)
+
+	return res
 }
 
-func FindAnagrams(dictionary []string, word string) (result []string) {
-	n := Normalize(word)
-	if len(n) == 0 {
-		return []string{}
-	}
-	var res []string
-	for _, s := range dictionary {
-		n2 := Normalize(s)
-		if n == n2 && !strings.EqualFold(s, word) {
-			res = append(res, s)
+func cleanInts(s string) []int {
+	// lowercase word
+	s = strings.ToLower(s)
+
+	// strip non alphanumeric
+	sBytes := []byte(s)
+	cleanedInts := make([]int, 0, len(s))
+	for _, b := range sBytes {
+		if b > byte(96) && b < byte(123) {
+			cleanedInts = append(cleanedInts, int(b))
 		}
 	}
-	return res
+
+	return cleanedInts
+}
+
+func areEqual(word, dic []int) bool {
+	for i := range word {
+		if word[i] != dic[i] {
+			return false
+		}
+	}
+	return true
 }
