65beeea09b80d6e47dd329177d73869e6a9a7fea
mick4711 <mick4711@gmail.com>
2024-10-24 00:26:02 +0100

    start recursive tree

--- lastlettergame/lastlettergame.go
+++ lastlettergame/lastlettergame.go
@@ -2,83 +2,91 @@ package lastlettergame
 
 import "fmt"
 
-// make a struct with word, startCount, endCount
+// make a struct with word parameters
 type wordParam struct {
-	startRune  rune
-	endRune    rune
-	startCount int
-	endCount   int
+	startRune rune
+	endRune   rune
+	isRoot    bool
+	done      bool
+	children  []wordParam
 }
 
 var wordParams map[string]*wordParam
 
 func Sequence(words []string) []string {
-	// make a map of word params with start and end letters and joining word counts
-	wordParams = getWordParams(words)
-	for word, wordParam := range wordParams {
-		fmt.Println(word, wordParam.startCount, wordParam.endCount)
+	// make a map of word params
+	wordParams = setWordParams(words)
+	nextWord := words[0]
+	for finished := false; !finished; {
+		children := getChildren(*wordParams[nextWord])
+		wordParams[nextWord].children = children
+		wordParams[nextWord].done = true
+
+		nextWord, finished = getNextWord()
 	}
 
-	// get longest chain for each wordParam (recursive?)
+	// debug print
 	for word, wordParam := range wordParams {
-		// getNextWords(*wordParam)
-		// for i := 0; i < wordParam.startCount; i++ {
-		// 	// get wordParam with matching start rune
-		// }
-		fmt.Println(word, getNextWords(*wordParam))
+		fmt.Println(word, *wordParam)
 	}
-	// find middle with positive start and end counts
-	// for word, wordParam := range wordParams {
-	// 	fmt.Println(word, wordParam.startCount, wordParam.endCount)
-	// }
-	// append valid start and end values
+
 	return []string{"ab", "bc", "cd"}
 	// return []string{"df", "fz", "zd", "dq", "qf", "fg"}
 }
 
-// get next words
-func getNextWords(wp wordParam) []string {
-	nextWords := []string{}
-
+// get next word that is not done
+func getNextWord() (string, bool) {
+	done := true
 	for word, wordParam := range wordParams {
-		if wp.endRune == wordParam.startRune {
-			nextWords = append(nextWords, word)
+		if wordParam.done == false {
+			return word, !done
 		}
 	}
-	return nextWords
+	return "", done
+}
+
+// get words that can be tacked onto passed in word
+func getChildren(wp wordParam) []wordParam {
+	if wp.done {
+		return wp.children
+	}
+
+	children := []wordParam{}
+	for word, wordParam := range wordParams {
+		if wp.endRune == wordParam.startRune {
+			wordParams[word].isRoot = false
+			children = append(children, *wordParams[word])
+		}
+	}
+
+	if len(children) == 0 {
+		return children
+	}
+
+	for _, wordParam := range children {
+		getChildren(wordParam)
+	}
+
+	return children
 }
 
 // make a map of word params with start and end letters and joining word counts
-func getWordParams(words []string) map[string]*wordParam {
+func setWordParams(words []string) map[string]*wordParam {
 	// initialise maps
 	wordParams := make(map[string]*wordParam, len(words))
-	// startRunes := make(map[rune]int)
-	// endRunes := make(map[rune]int)
 
 	// fill map with start and end letters keeping counts
 	for _, word := range words {
 		startRune := rune(word[0])
-		// startRunes[startRune]++
 		endRune := rune(word[len(word)-1])
-		// endRunes[endRune]++
 		wordParams[word] = &wordParam{
-			startRune,
-			endRune,
-			0,
-			0,
+			startRune: startRune,
+			endRune:   endRune,
+			isRoot:    true,
+			done:      false,
+			children:  []wordParam{},
 		}
 	}
-	// fmt.Println(startRunes, endRunes)
-
-	// update map with start and end counts
-	// for _, wordParam := range wordParams {
-	// 	if count, ok := endRunes[wordParam.startRune]; ok {
-	// 		wordParam.endCount = count
-	// 	}
-	// 	if count, ok := startRunes[wordParam.endRune]; ok {
-	// 		wordParam.startCount = count
-	// 	}
-	// }
 
 	return wordParams
 }
