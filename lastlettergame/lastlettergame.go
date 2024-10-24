2317ae0a1674787323c3e980ed0462367ffaec65
mick4711 <mick4711@gmail.com>
2024-10-24 18:43:10 +0100

    get root words

--- lastlettergame/lastlettergame.go
+++ lastlettergame/lastlettergame.go
@@ -8,21 +8,25 @@ type wordParam struct {
 	endRune   rune
 	isRoot    bool
 	done      bool
-	children  []wordParam
+	children  []string
 }
 
-var wordParams map[string]*wordParam
+// var wordParams map[string]*wordParam
 
 func Sequence(words []string) []string {
 	// make a map of word params
-	wordParams = setWordParams(words)
-	nextWord := words[0]
-	for finished := false; !finished; {
-		children := getChildren(*wordParams[nextWord])
-		wordParams[nextWord].children = children
-		wordParams[nextWord].done = true
+	wordParams := setWordParams(words)
 
-		nextWord, finished = getNextWord()
+	// populate children
+	populateChildren(words, wordParams)
+
+	// get words that have no parents
+	rootWords := getRootWords(wordParams)
+
+	// for each rootWord get longest sequence
+	var sequence []string
+	for _, rootWord := range rootWords {
+		sequence = getLongestSeq(rootWord, wordParams)
 	}
 
 	// debug print
@@ -30,47 +34,11 @@ func Sequence(words []string) []string {
 		fmt.Println(word, *wordParam)
 	}
 
-	return []string{"ab", "bc", "cd"}
+	return sequence
+	// return []string{"ab", "bc", "cd"}
 	// return []string{"df", "fz", "zd", "dq", "qf", "fg"}
 }
 
-// get next word that is not done
-func getNextWord() (string, bool) {
-	done := true
-	for word, wordParam := range wordParams {
-		if wordParam.done == false {
-			return word, !done
-		}
-	}
-	return "", done
-}
-
-// get words that can be tacked onto passed in word
-func getChildren(wp wordParam) []wordParam {
-	if wp.done {
-		return wp.children
-	}
-
-	children := []wordParam{}
-	for word, wordParam := range wordParams {
-		if wp.endRune == wordParam.startRune {
-			wordParams[word].isRoot = false
-			children = append(children, *wordParams[word])
-		}
-	}
-
-	if len(children) == 0 {
-		return children
-	}
-
-	for _, wordParam := range children {
-		getChildren(wordParam)
-	}
-
-	return children
-}
-
-// make a map of word params with start and end letters and joining word counts
 func setWordParams(words []string) map[string]*wordParam {
 	// initialise maps
 	wordParams := make(map[string]*wordParam, len(words))
@@ -84,9 +52,73 @@ func setWordParams(words []string) map[string]*wordParam {
 			endRune:   endRune,
 			isRoot:    true,
 			done:      false,
-			children:  []wordParam{},
+			children:  []string{},
 		}
 	}
 
 	return wordParams
+}
+
+func populateChildren(words []string, wordParams map[string]*wordParam) {
+	nextWord := words[0]
+	for finished := false; !finished; {
+		children := getChildren(*wordParams[nextWord], wordParams)
+		wordParams[nextWord].children = children
+		wordParams[nextWord].done = true
+
+		nextWord, finished = getNextWord(wordParams)
+	}
+}
+
+// get words that can be tacked onto passed in word
+func getChildren(wp wordParam, wordParams map[string]*wordParam) []string {
+	if wp.done {
+		return wp.children
+	}
+
+	children := []string{}
+	for word, wordParam := range wordParams {
+		if wp.endRune == wordParam.startRune {
+			wordParams[word].isRoot = false
+			children = append(children, word)
+		}
+	}
+
+	for _, word := range children {
+		getChildren(*wordParams[word], wordParams)
+	}
+
+	return children
+}
+
+// get next word that is not done
+func getNextWord(wordParams map[string]*wordParam) (string, bool) {
+	done := true
+	for word, wordParam := range wordParams {
+		if wordParam.done == false {
+			return word, !done
+		}
+	}
+	return "", done
+}
+
+func getRootWords(wordParams map[string]*wordParam) []string {
+	rootWords := []string{}
+	for word, wordParam := range wordParams {
+		if wordParam.isRoot {
+			rootWords = append(rootWords, word)
+		}
+	}
+
+	return rootWords
+}
+
+func getLongestSeq(word string, wordParams map[string]*wordParam) []string {
+	seq := []string{}
+	for _, child := range wordParams[word].children {
+		seq = append(seq, child)
+	}
+
+	// return seq
+	return []string{"ab", "bc", "cd"}
 }
